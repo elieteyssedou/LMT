0806a4bc26044ebee334c7b643ac6ee2b98beb89
Elie Teyssedou <elieteyssedou@gmail.com>
2016-03-11 23:30:31 +0100

    Changes gem name to LMT, ambigous name

--- lib/livementor_test.rb
+++
@@ -1,7 +0,0 @@
-require "livementor_test/json_parser"
-require "livementor_test/csv_generator"
-require "livementor_test/cli"
-require "livementor_test/version"
-
-module LivementorTest
-end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,4 +1,4 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
-require 'livementor_test'
+require 'lmt'
 require 'minitest'
 require 'minitest/autorun'
